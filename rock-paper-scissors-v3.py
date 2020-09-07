f8dd193b7d3ebc367ad878c6183d67f4d084657a
brandong1 <brandon@brandonleeproductions.com>
2020-09-06 19:50:45 -0600

    move code to correct file

--- rock-paper-scissors-v3.py
+++ rock-paper-scissors-v3.py
@@ -7,12 +7,21 @@ print("***NO CHEATING!\n\n" * 20)
 player2 = input("Player 2, make your move: ")
  
 if player1 == player2:
-    print("Draw")
-elif player1 == "rock" and player2 == "scissors":
-    print("player1 wins")
-elif player1 == "paper" and player2 == "rock":
-    print("player1 wins")
-elif player1 == "scissors" and player2 == "paper":
-    print("player1 wins")
+	print("It's a tie!")
+elif player1 == "rock":
+	if player2 == "scissors":
+		print("player1 wins!")
+	elif player2 == "paper":
+		print("player2 wins!")
+elif player1 == "paper":
+	if player2 == "rock":
+		print("player1 wins!")
+	elif player2 == "scissors":
+		print("player2 wins!")
+elif player1 == "scissors":
+	if player2 == "paper":
+		print("player1 wins!")
+	elif player2 == "rock":
+		print("player2 wins!")	
 else:
-    print("player2 wins")
+	print("something went wrong")
