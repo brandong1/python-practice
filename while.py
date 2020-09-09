7c69c35345b7bc41c1d331e4d5befa322571206d
brandong1 <brandon@brandonleeproductions.com>
2020-09-09 08:46:40 -0600

    while loop practice

---
+++ while.py
@@ -0,0 +1,13 @@
+# message = input("What's the secret password?")
+# while message != "bananas":
+#     print("WRONG!")
+#     message = input("What's the secret password?")
+# print("CORRECT!")
+
+# for num in range(1,11):
+#     print(num)
+
+num = 1
+while num < 11:
+    print(num)
+    num += 1
