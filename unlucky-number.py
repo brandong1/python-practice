66ca1b1bd77087a2f07fcd0b1d8909b03340a089
brandong1 <brandon@brandonleeproductions.com>
2020-09-08 21:21:01 -0600

    refactored to remove excessive print statements

--- unlucky-number.py
+++ unlucky-number.py
@@ -6,3 +6,13 @@ for num in range(1,21):
     else:
         print(f"{num} is odd")
 
+###########################
+
+for num in range(1,21):
+    if num == 4 or num == 13:
+        state = "unlucky"
+    elif num % 2 == 0:
+        state = "even"
+    else:
+        state = "odd"
+    print(f"{num} is {state}")
