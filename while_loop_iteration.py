1ff586f09214f569616a88a71e0efca29ae79a28
brandong1 <brandon@brandonleeproductions.com>
2020-09-10 13:54:45 -0600

    while loop practice

---
+++ while_loop_iteration.py
@@ -0,0 +1,14 @@
+# numbers = [1,2,3,4]
+# i = 0
+
+# while i < len(numbers):
+#     print(numbers[i])
+#     num += 1
+
+colors = ["purple", "teal", "red", "orange", "emerald"]
+
+index = 0
+while index < len(colors):
+    print(f"{index}: {colors[index]}")
+    index += 1
+
