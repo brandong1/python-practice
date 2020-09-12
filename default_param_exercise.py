013d95b01b26414dd10e3177a182824a8f873499
brandong1 <brandon@brandonleeproductions.com>
2020-09-11 21:02:17 -0600

    default params practice

---
+++ default_param_exercise.py
@@ -0,0 +1,25 @@
+def speak(animal="dog"):
+    if animal == "pig":
+        return "oink"
+    elif animal == "duck":
+         return "quack"
+    elif animal == "cat":
+        return "meow"
+    elif animal == "dog":
+        return "woof"
+    else:
+        return "?"
+
+##########
+def speak(animal="dog"):
+    noises = {"dog": "woof", "pig": "oink", "duck": "quack", "cat": "meow"}
+    noise = noises.get(animal)
+    if noise:
+        return noise
+    return "?"
+
+##########
+
+def speak(animal='dog'):
+    noises = {'pig':'oink', 'duck':'quack', 'cat':'meow', 'dog':'woof'}
+    return noises.get(animal, '?')
