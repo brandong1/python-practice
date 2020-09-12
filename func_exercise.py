a6046be0ab9a3d8c678cbd2a040df941f46e6789
brandong1 <brandon@brandonleeproductions.com>
2020-09-11 21:02:40 -0600

    function with list comp practice

---
+++ func_exercise.py
@@ -0,0 +1,4 @@
+#Define a function called generate_evens
+#It should return a list of even numbers between 1 and 50(not including 50)
+def generate_evens():
+    return [x for x in range(1,50) if x % 2 == 0]
