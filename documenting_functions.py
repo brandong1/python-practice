c27a66a0f97421b398e02ef40158ecf04cc5438c
brandong1 <brandon@brandonleeproductions.com>
2020-09-11 21:16:02 -0600

    documenting functions notes

---
+++ documenting_functions.py
@@ -0,0 +1,7 @@
+def say_hello():
+    """A simple function that returns the string hello"""
+    return "Hello!"
+
+say_hello.__doc__ # 'A simple function that returns the string hello'
+
+# All built-in function have this
