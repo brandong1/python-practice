a5586c1e61f493e057c7f44697a6be9a3a46f640
brandong1 <brandon@brandonleeproductions.com>
2020-09-14 20:40:15 -0600

    various ways to print a string

---
+++ print_string_format.py
@@ -0,0 +1,10 @@
+first_name = "John"
+last_name = "Doe"
+
+print("My full name is {} {}".format(first_name, last_name))
+
+print("My full name is {first_name} {last_name}")
+
+print("My full name is " + first_name + " " + last_name)
+
+print("My full name is", first_name, last_name)
