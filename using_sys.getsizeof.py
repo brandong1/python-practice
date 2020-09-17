51e770c0cc64d5387fea6aced726d7513a7616ca
brandong1 <brandon@brandonleeproductions.com>
2020-09-17 16:20:51 -0600

    how to use sys.getsizeof() to measure performance

---
+++ using_sys.getsizeof.py
@@ -0,0 +1,8 @@
+import sys
+# A simple comparison of size (in Bytes)
+list_comp = sys.getsizeof([x * 10 for x in range(1000)]) # 9024 bytes
+gen_exp = sys.getsizeof(x * 10 for x in range(1000)) # 88 bytes
+
+print("To do the same thing, it takes...")
+print(f"List Comprehension: {list_comp} bytes")
+print(f"Generator Expression: {gen_exp} bytes")
