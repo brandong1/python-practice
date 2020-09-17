3bd559bb2763324364fefe34b7333c8b7d5ca659
brandong1 <brandon@brandonleeproductions.com>
2020-09-17 16:23:39 -0600

    classes exercise

---
+++ classes_and_objects.py
@@ -0,0 +1,38 @@
+class Student():
+    def __init__(self, first, last, courses=None):
+        self.first_name = first
+        self.last_name = last
+        if courses == None:
+            courses = []
+        else:
+            self.courses = courses
+
+    def add_course(self, course):
+        if course not in self.courses:
+            self.courses.append(course)
+        else:
+            print(f"{self.first_name} is already enrolled in the {course} course.")
+
+    def remove_course(self, course):
+        if course in self.courses:
+            self.courses.remove(course)
+        else:
+            print(f"{course} not found!")
+
+
+courses_1 = ['python', 'rails', 'javascript']
+courses_2 = ['java', 'rails', 'c']
+brandon = Student("brandon", "green", courses_1)
+john = Student("john", "doe", courses_2)
+
+print(brandon.first_name, brandon.last_name, brandon.courses)
+brandon.add_course("swift")
+brandon.add_course("python")
+brandon.remove_course("python")
+brandon.remove_course("c++")
+
+print(brandon.first_name, brandon.last_name, brandon.courses)
+
+# brandon green ['python', 'rails', 'javascript']
+print(john.first_name, john.last_name, john.courses)
+# john doe ['java', 'rails', 'c']
