89bb656adc09d2b6aece02b80832beaf2360c352
brandong1 <brandon@brandonleeproductions.com>
2020-09-17 16:21:26 -0600

    sum even values exercise

---
+++ sum_even_values.py
@@ -0,0 +1,14 @@
+# Sum Even Values Solution
+# I define a function called sum_even_values  which accepts any number of arguments, thanks to *args 
+# I grab the even numbers using the generator expression (arg for arg in args if arg % 2 == 0)  (could also use a list comp)
+# I pass the even numbers I extracted into sum()  and return the value
+# The default start value of sum()  is 0, so I don't have to do anything special to get it to return 0 if there are no even numbers!
+'''
+sum_even_values(1,2,3,4,5,6) # 12
+sum_even_values(4,2,1,10) # 16
+sum_even_values(1) # 0
+'''
+
+# define sum_even_values
+def sum_even_values(*args):
+    return sum(arg for arg in args if arg % 2 == 0)
