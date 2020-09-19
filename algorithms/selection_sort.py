8f54635959d6cb90fb9aa1609741edf31ef63b8b
brandong1 <brandon@brandonleeproductions.com>
2020-09-19 11:59:56 -0600

    selection sort implementation

---
+++ algorithms/selection_sort.py
@@ -0,0 +1,18 @@
+# Selection Sort
+# Time complexity: O(n^2)
+# Space Complexity: O(1)
+
+# The algorithm divides the input list into two parts: a sorted sublist of items which is built up from left to right at the front (left) of the list and a sublist of the remaining unsorted items that occupy the rest of the list. Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by finding the smallest (or largest, depending on sorting order) element in the unsorted sublist, exchanging (swapping) it with the leftmost unsorted element (putting it in sorted order), and moving the sublist boundaries one element to the right.
+
+# The time efficiency of selection sort is quadratic, so there are a number of sorting techniques which have better time complexity than selection sort. One thing which distinguishes selection sort from other sorting algorithms is that it makes the minimum possible number of swaps, n − 1 in the worst case.
+def selection_sort(arr):
+    spot_marker = 0
+    while spot_marker < len(arr):
+        for num in range(spot_marker, len(arr)):
+            if arr[num] < arr[spot_marker]:
+                arr[spot_marker], arr[num] = arr[num], arr[spot_marker]
+        spot_marker += 1
+        print(arr)
+
+l = [6,8,1,4,10,7,8,9,3,2,5]
+selection_sort(l)
