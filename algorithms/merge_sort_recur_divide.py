4925e8973ad29f4132f08624ed45d7cddb5f4d4a
brandong1 <brandon@brandonleeproductions.com>
2020-09-21 20:52:03 -0600

    merge sort recursion

---
+++ algorithms/merge_sort_recur_divide.py
@@ -0,0 +1,11 @@
+def divide_arr(arr):
+    if len(arr) < 2:
+        return arr[:]
+    else:
+        middle = len(arr)//2
+        l1 = divide_arr(arr[:middle])
+        l2 = divide_arr(arr[middle:])
+        # implied return None
+
+l = [8,6,2,5]
+divide_arr(l)
