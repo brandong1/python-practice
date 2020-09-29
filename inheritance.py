55ad19e0cf0793a9eb5e886500c6aa60e1bffe8d
brandong1 <brandon@brandonleeproductions.com>
2020-09-29 16:09:16 -0600

    inheritance example

---
+++ inheritance.py
@@ -0,0 +1,10 @@
+class Character:
+    def __init__(self,name,hp,level):
+        self.name = name
+        self.hp = hp
+        self.level = level
+    
+    
+class NPC(Character):
+    def speak():
+        print("Leeroooooy Jeeenkiiiiins!")
